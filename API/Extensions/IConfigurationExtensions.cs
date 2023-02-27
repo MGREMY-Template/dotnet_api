51e84a205f2e368b6046e6a5a246fdaa1fcf7ac9
Miguel GREMY <mgremy@gremy.ovh>
2023-02-27 21:40:53 +0100

    Fix get environment variable Update Dockerfile

--- API/Extensions/IConfigurationExtensions.cs
+++ API/Extensions/IConfigurationExtensions.cs
@@ -1,14 +1,15 @@
 ﻿using Microsoft.Extensions.Configuration;
+using System.Linq;
 
 namespace API.Extensions
 {
-	public static class IConfigurationExtensions
-	{
-		public static string GetFromEnvironmentVariable(
-			this IConfiguration configuration,
-			params string[] keys)
-		{
-			return configuration[string.Join(":", "APP_CFG", keys)];
-		}
-	}
+    public static class IConfigurationExtensions
+    {
+        public static string GetFromEnvironmentVariable(
+            this IConfiguration configuration,
+            params string[] keys)
+        {
+            return configuration[string.Join(":", keys.Prepend("APP_CFG"))];
+        }
+    }
 }
