30a97b6942bb6a5f17753600bbc156daa64c0115
MGREMY <126514209+MGREMY@users.noreply.github.com>
2023-03-12 13:52:13 +0100

    Use AutoMapper

---
+++ Shared.Core/Mapper/Identity/UserMapper.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Shared.Core.DataTransferObject.Identity.UserController;
+using Shared.Core.Entities.Identity;
+
+namespace Shared.Core.Mapper.Identity
+{
+    public class UserMapper : Profile
+    {
+        public UserMapper()
+        {
+            CreateMap<User, UserDto>()
+                .ForMember(x =>
+                    x.Id,
+                    opt => opt.MapFrom(src => src.Id))
+                .ForMember(x =>
+                    x.Email,
+                    opt => opt.MapFrom(src => src.Email))
+                .ForMember(x =>
+                    x.UserName,
+                    opt => opt.MapFrom(src => src.UserName))
+                .ReverseMap();
+        }
+    }
+}
