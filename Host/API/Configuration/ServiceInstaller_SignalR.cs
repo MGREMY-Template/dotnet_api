ec3a4b719c86643c643c63e6610c68eb0b86000e
MGREMY <126514209+MGREMY@users.noreply.github.com>
2023-04-10 22:47:24 +0200

    Add NotificationHub and Swagger generation (#59)

---
+++ Host/API/Configuration/ServiceInstaller_SignalR.cs
@@ -0,0 +1,24 @@
+﻿namespace API.Configuration;
+
+using API.Hubs;
+using Domain.Attributes;
+using Domain.Configuration;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[ConfigOrder(0)]
+public class ServiceInstaller_SignalR : IServiceInstaller
+{
+    public void Configure(IServiceCollection services, IConfiguration configuration)
+    {
+        _ = services.AddSignalR()
+            .AddJsonProtocol();
+    }
+
+    public void Install(IApplicationBuilder applicationBuilder)
+    {
+        ((IEndpointRouteBuilder) applicationBuilder).MapHub<NotificationHub>("hub/notification");
+    }
+}
---
+++ Host/API/Hubs/NotificationHub.cs
@@ -0,0 +1,99 @@
+﻿namespace API.Hubs;
+
+using AutoMapper;
+using Domain.DataTransferObject.Identity.RoleController;
+using Domain.DataTransferObject.Identity.UserController;
+using Domain.Entities.Identity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
+using SignalRSwaggerGen.Attributes;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+[SignalRHub]
+[Authorize]
+public class NotificationHub : Hub
+{
+    private readonly string _notificationMethod = "Notification";
+
+    private readonly UserManager<User> _userManager;
+    private readonly ILogger _logger;
+    private readonly IMapper _mapper;
+
+    public NotificationHub(
+        UserManager<User> userManager,
+        ILogger<NotificationHub> logger,
+        IMapper mapper)
+    {
+        this._userManager = userManager;
+        this._logger = logger;
+        this._mapper = mapper;
+    }
+
+    public override async Task OnConnectedAsync()
+    {
+        var user = await this._userManager.GetUserAsync(this.Context.User);
+        var roles = await this._userManager.GetRolesAsync(user);
+
+        foreach (var role in roles)
+        {
+            await this.Groups.AddToGroupAsync(user.Id.ToString(), role);
+        }
+
+        await base.OnConnectedAsync();
+    }
+
+    public override async Task OnDisconnectedAsync(Exception exception)
+    {
+        if (exception is not null)
+        {
+            this._logger.LogWarning(exception: exception, message: null, args: null);
+        }
+
+        var user = await this._userManager.GetUserAsync(this.Context.User);
+        var roles = await this._userManager.GetRolesAsync(user);
+
+        foreach (var role in roles)
+        {
+            await this.Groups.RemoveFromGroupAsync(user.Id.ToString(), role);
+        }
+
+        await base.OnDisconnectedAsync(exception);
+    }
+
+    public async Task NotifyAll(string title, string message)
+    {
+        await this.Clients.All.SendAsync(this._notificationMethod, title, message);
+    }
+
+    public async Task NotifyUser(string title, string message, UserDto user)
+    {
+        await this.Clients.User(this._mapper.Map<User>(user).Id.ToString())
+            .SendAsync(this._notificationMethod, title, message);
+    }
+
+    public async Task NotifyUsers(string title, string message, params UserDto[] users)
+    {
+        await this.Clients.Users(this._mapper.Map<User[]>(users).Select(x =>
+        {
+            return x.Id.ToString();
+        })).SendAsync(this._notificationMethod, title, message);
+    }
+
+    public async Task NotifyRole(string title, string message, RoleDto role)
+    {
+        await this.Clients.Group(this._mapper.Map<Role>(role).Name)
+            .SendAsync(this._notificationMethod, title, message);
+    }
+
+    public async Task NotifyRoles(string title, string message, params RoleDto[] roles)
+    {
+        await this.Clients.Groups(this._mapper.Map<Role[]>(roles).Select(x =>
+        {
+            return x.Name;
+        })).SendAsync(this._notificationMethod, title, message);
+    }
+}
