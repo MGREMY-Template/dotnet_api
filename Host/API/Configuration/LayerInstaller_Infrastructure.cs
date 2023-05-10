ff5e332f59e7521ebf1fb97c471ff3c42578bdab
MGREMY <126514209+MGREMY@users.noreply.github.com>
2023-05-10 19:25:56 +0200

    Add initial user by environment variable

--- Host/API/Configuration/LayerInstaller_Infrastructure.cs
+++ Host/API/Configuration/LayerInstaller_Infrastructure.cs
@@ -10,8 +10,11 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using MySqlConnector;
 using Domain.Attributes;
+using Domain.Entities.Identity;
+using System;
+using Microsoft.AspNetCore.Identity;
 
-[ConfigOrder(0)]
+[ConfigOrder(1)]
 public class LayerInstaller_Infrastructure : IServiceInstaller
 {
     public void Configure(IServiceCollection services, IConfiguration configuration)
@@ -34,10 +37,41 @@ public class LayerInstaller_Infrastructure : IServiceInstaller
         IdentityContext identityContext = scope.GetRequiredService<IdentityContext>();
         identityContext.Database.Migrate();
 
+        if (identityContext.AppSettings.Find(Domain.Constants.AppSettingConstants.IS_INITIALIZED).Value.Equals("0"))
+        {
+            InitializeData(identityContext, configuration, scope.GetRequiredService<IPasswordHasher<User>>());
+        }
+
         _ = services.AddScoped<IAppDbContext>(provider =>
         {
             return provider.GetService<IdentityContext>();
         });
+
+        static void InitializeData(IdentityContext context, IConfiguration configuration, IPasswordHasher<User> passwordHasher)
+        {
+            var user = new User
+            {
+                Id = Guid.NewGuid(),
+                UserName = configuration.GetFromEnvironmentVariable("INIT", "USER", "USERNAME") ?? "admin",
+                NormalizedUserName = configuration.GetFromEnvironmentVariable("INIT", "USER", "USERNAME")?.ToUpper()?.Normalize() ?? "admin".Normalize(),
+                Email = configuration.GetFromEnvironmentVariable("INIT", "USER", "EMAIL") ?? "admin@admin.admin",
+                NormalizedEmail = configuration.GetFromEnvironmentVariable("INIT", "USER", "EMAIL")?.ToUpper()?.Normalize() ?? "admin@admin.admin".Normalize(),
+                EmailConfirmed = true,
+            };
+
+            user.PasswordHash = passwordHasher.HashPassword(user, configuration.GetFromEnvironmentVariable("INIT", "USER", "PASSWORD") ?? "password");
+
+            foreach (var claim in typeof(Domain.Constants.ClaimDefinition).GetAllPublicConstantValues<string>())
+            {
+                context.UserClaims.Add(new UserClaim { UserId = user.Id, ClaimType = claim, ClaimValue = "1" });
+            }
+
+            context.Users.Add(user);
+
+            context.AppSettings.Find(Domain.Constants.AppSettingConstants.IS_INITIALIZED).Value = "1";
+
+            context.SaveChanges();
+        }
     }
 
     public void Install(IApplicationBuilder applicationBuilder)
