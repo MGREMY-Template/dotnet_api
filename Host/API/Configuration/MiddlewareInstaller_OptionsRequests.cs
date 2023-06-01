c4a52307733e9d89b2d9ff1fb74edaa018bf870c
MGREMY <126514209+MGREMY@users.noreply.github.com>
2023-06-01 17:21:36 +0200

    Add CORS policy (#88)

---
+++ Host/API/Configuration/MiddlewareInstaller_OptionsRequests.cs
@@ -0,0 +1,26 @@
+﻿namespace API.Configuration;
+
+using Domain.Configuration;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+public class MiddlewareInstaller_OptionsRequests : IServiceInstaller
+{
+    public void Configure(IServiceCollection services, IConfiguration configuration)
+    {
+        _ = services.AddCors(opt =>
+        {
+            opt.AddDefaultPolicy(policy =>
+            {
+                policy.AllowAnyOrigin();
+                policy.AllowAnyHeader();
+            });
+        });
+    }
+
+    public void Install(IApplicationBuilder applicationBuilder)
+    {
+        applicationBuilder.UseCors();
+    }
+}
