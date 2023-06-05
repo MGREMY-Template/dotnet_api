4107dcd0442d9b39c5b5c6b8b5c639cd30bfc814
MGREMY <126514209+MGREMY@users.noreply.github.com>
2023-06-05 21:54:52 +0200

    Add AppFile

---
+++ Core/Domain/Entities/Application/AppFile.cs
@@ -0,0 +1,18 @@
+﻿namespace Domain.Entities.Application;
+
+using Domain.Entities;
+using Domain.Entities.Identity;
+using System;
+using System.IO;
+
+public class AppFile : IBaseEntity<Guid>
+{
+    public Guid Id { get; set; }
+    public DateTime CreationDate { get; set; }
+    public string MimeType { get; set; }
+    public int Size { get; set; }
+    public Stream Content { get; set; }
+    public Guid UserId { get; set; }
+
+    public User CreationUser { get; set; }
+}
--- Core/Domain/Entities/Identity/User.cs
+++ Core/Domain/Entities/Identity/User.cs
@@ -1,10 +1,14 @@
 ﻿namespace Domain.Entities.Identity;
 
 using Domain.Entities;
+using Domain.Entities.Application;
 using Microsoft.AspNetCore.Identity;
 using System;
+using System.Collections.Generic;
 
 public partial class User : IdentityUser<Guid>, IBaseEntity<Guid>
 {
     public override Guid Id { get; set; }
+
+    public virtual IEnumerable<AppFile> Files { get; set; }
 }
